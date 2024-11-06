9d6aca050e4c11c079880204c36e1db776a17e90
Fernando <fernandogui2004@gmail.com>
2024-11-06 09:08:27 -0300

    alterando tamanho do campo nomeDisciplina para varchar 150

--- database/migrations/2024_10_12_000003_create_disciplinas_table.php
+++ database/migrations/2024_10_12_000003_create_disciplinas_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('disciplinas', function (Blueprint $table) {
             $table->id('idDisciplina');
-            $table->string('nomeDisciplina',50);
+            $table->string('nomeDisciplina',150);
             $table->timestamps();
         });
 
